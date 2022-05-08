bc58b4e46f80c4b6ce1091d3a308a16fef5c1f8d
pedronarbondo <pedro.narbondo@hotmail.com>
2022-05-07 21:41:49 -0300

    Push last update before sleep. Very close

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-
-
-/* Create function that returns a random choice between the three possible */
 function computerPlay() {
     let possible = ["Rock", "Paper", "Scissors"];
     let number = Math.floor(Math.random() * (3));
@@ -8,48 +5,82 @@ function computerPlay() {
     return computerSelected
 }
 
+/* Incredibly this next function worked on the first time */
+function changeComputerPhoto(){
+    computerPlay();
+    let computerChoice = computerPlay();
+    switch (computerChoice) {
+    case "rock":
+        computerPhoto.src="img/rock.png";
+        return computerChoice;
+    case "paper":
+        computerPhoto.src="img/paper.png";
+        return computerChoice;
+    case "scissors":
+        computerPhoto.src="img/scissors.png";
+        return computerChoice;
+    }
+}
+
+/* This next group is everything related to the buttons and the chosen-item images */
 let rockButton = document.querySelector(".rock");
 let paperButton =  document.querySelector(".paper");
 let scissorsButton = document.querySelector(".scissors");
-let playerPhoto = document.querySelector(".player").querySelector("img")
 
-rockButton.addEventListener('click', () => playerPhoto.src="img/rock.png");
-paperButton.addEventListener('click', () => playerPhoto.src="img/paper.png");
-scissorsButton.addEventListener('click', () => playerPhoto.src="img/scissors.png");
+let playerPhoto = document.querySelector(".player").querySelector("img");
+let computerPhoto = document.querySelector(".computer").querySelector("img");
 
 
+function playerPlay(){
+let playerChoice; 
+rockButton.addEventListener('click', () => {playerPhoto.src="img/rock.png", playerChoice = "rock"});
+paperButton.addEventListener('click', () => {playerPhoto.src="img/paper.png", playerChoice = "paper"});
+scissorsButton.addEventListener('click', () => {playerPhoto.src="img/scissors.png", playerChoice = "scissors"});
+return playerChoice;
+}
 
-/* Assign the values of the functions previously defined to variables to use in future functions*/
-/*let computerSelection = computerPlay();
-let playerSelection = playerPlay();
-*/
 
-/* Create a function that returns a string that explains who wins */
-function playRound(playerSelection, computerSelection) {
-       if (playerSelection === computerSelection){
-        return "It's a tie!"
+
+
+/* This group of references is for the top part of the UI, with the status */
+let playerScoreText = document.querySelector(".player").querySelector("p");
+let computerScoreText = document.querySelector(".computer").querySelector("p");
+let statusText = document.querySelector('p');
+
+
+let playerScore = 0;
+let computerScore = 0;
+
+
+function playRps(){
+for (let i = 0; playerScore <= 5 || computerScore <= 5; i++){
+
+playerPlay();
+changeComputerPhoto();
+
+    if (changeComputerPhoto() === playerPlay()) {
+        statusText.textContent = "It's a tie!"
     }
     else if
-           (
-            (playerSelection === "rock" && computerSelection === "scissors") ||
-            (playerSelection === "paper" && computerSelection === "rock") ||
-            (playerSelection === "scissors" && computerSelection === "paper")
-            )
-            {
-                return "youWin"
-             }
-    else if
-            (
-            (playerSelection === "rock" && computerSelection === "paper") ||
-            (playerSelection === "paper" && computerSelection === "scissors") ||
-            (playerSelection === "scissors" && computerSelection === "rock")
-            )
-             return "youLose"
-        
-        if (playRound(playerSelection, computerSelection) === "youWin") {
-                console.log("You win; " + playerSelection + " beats " + computerSelection + "!")
-            }
-        else if (playRound(playerSelection, computerSelection) === "youLose") {
-                console.log("You lose; " + computerSelection + " beats " + playerSelection + ".")
-            }                
+    (
+     (playerPlay() === "rock" && changeComputerPhoto() === "scissors") ||
+     (playerPlay() === "paper" && changeComputerPhoto() === "rock") ||
+     (playerPlay() === "scissors" && changeComputerPhoto() === "paper")
+     )
+     {
+        statusText.textContent = "You win one point!";
+        playerScore++;
+        playerScoreText.textContent = `Player: ${playerScore}`
+     }
+     else if
+     (
+     (playerPlay() === "rock" && changeComputerPhoto() === "paper") ||
+     (playerPlay() === "paper" && changeComputerPhoto() === "scissors") ||
+     (playerPlay() === "scissors" && changeComputerPhoto() === "rock")
+     ){
+     statusText.textContent = "The computer wins one point!";
+     computerScore++;
+     computerScoreText.textContent = `Computer: ${computerScore}`;
+     }
+    }
 }
